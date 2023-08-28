f4f000653ad8fe490a4e011cc9ef1decf8913990
mistyovo <3240827896@qq.com>
2023-08-29 00:00:40 +0800

    Site updated: 2023-08-29 00:00:35

--- js/sidebarFixed.js
+++ js/sidebarFixed.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', function () {
         var sidebar = document.querySelector('#sidebar');
         var scrollPosition = window.scrollY || document.documentElement.scrollTop;
-        if (scrollPosition >= 150) {
+        if (scrollPosition >= 160) {
             sidebar.classList.add('fixed');
         } else {
             sidebar.classList.remove('fixed');
